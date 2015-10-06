6c0676e046db0a2c10bd54117dd27068bb1c0632
Karan Sharma <karan1276@gmail.com>
2015-10-06 12:13:43 +0530

    Wrote a prog to overload assignment oprator

--- oprator overloading/assignmentOprator.cpp
+++ oprator overloading/assignmentOprator.cpp
@@ -6,6 +6,22 @@ class  Distance{
 		int inch;
 	public:
 		void display(){
-			cout<<"\n feet:"<<feet" inches:"<<inch;
+			cout<<"\n feet:"<<feet<<" inches:"<<inch;
+		}
+		void setval(int f,int i){
+			feet=f;
+			inch=i;
+		}
+		Distance operator = (Distance d){
+			feet = d.feet;
+			inch = d.inch;
 		}
 };
+int main(){
+	Distance d1,d2;
+	d1.setval(2,2);
+	d1.display();
+	d2=d1;
+	d2.display();
+	return 0;
+}
